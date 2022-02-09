690af899779bc4d211586da854eae96adddd75ec
cristianlm <cristian.lobomartin@zooplus.com>
2022-02-09 19:43:00 +0100

    Inject translation service

--- server/src/main/kotlin/es/cristianlm/route/HomeRoutes.kt
+++ server/src/main/kotlin/es/cristianlm/route/HomeRoutes.kt
@@ -1,5 +1,7 @@
 package es.cristianlm.route
 
+import es.cristianlm.domain.service.TranslationService
+import es.cristianlm.model.Language
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
@@ -7,7 +9,7 @@ import io.ktor.thymeleaf.*
 import javax.inject.Inject
 
 class HomeRoutes @Inject constructor(
-
+    private val translationService: TranslationService
 ) : AppRoute {
     override fun Route.configure() {
         get("/") {
