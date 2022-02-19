a9e1cc083f7e3ab64e9de12a8d9ed5f26e82a9da
cristianlm <cristian.lobomartin@zooplus.com>
2022-02-19 18:58:33 +0100

    Animate background text

--- server/src/main/resources/assets/js/background.js
+++ server/src/main/resources/assets/js/background.js
@@ -4,11 +4,8 @@ window.onload = function () {
     canvas.height = document.body.scrollHeight;
     canvas.width = document.body.scrollWidth;
     const ctx = canvas.getContext('2d');
-    ctx.font = "12px Arial";
-    // ctx.textAlign = "center";
-    ctx.fillStyle = "#AAA";
-    let x, y = 0;
-    let lineHeigth = 10;
+    ctx.font = "12px Monospace";
+    ctx.fillStyle = "#cee7ee";
     fillTextCenter(ctx, html, canvas.width, canvas.height)
 }
 
@@ -16,11 +13,38 @@ function fillTextCenter(ctx, text, width, height) {
     ctx.textBaseline = 'middle';
     ctx.textAlign = "center";
 
-    const lines = text.match(/[^\r\n]+/g);
-    for (let i = 0; i < lines.length; i++) {
-        let x = Math.round(Math.random() * (width - 50))
-        let y = (height / (lines.length + 1)) * (i + 1)
+    const lines = text
+        .match(/[^\r\n]+/g)
+        .map(line => line.trim());
+    const fps = 50;
+    const positions = []
+    setInterval(() => {
+        ctx.save();
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+        ctx.clearRect(0, 0, width, height);
+        ctx.restore();
+        for (let i = 0; i < lines.length; i++) {
+            if (!positions[i]) {
+                const x = Math.round(Math.random() * (width - 50));
+                const y = (height / (lines.length + 1)) * (i + 1);
+                const speed = Math.random() + 0.1;
+                const direction = Math.random() > 0.5 ? 'r' : 'l';
+                positions[i] = {x, y, speed, direction};
+            } else {
+                switch (positions[i].direction) {
+                    case 'r':
+                        positions[i].x += positions[i].speed;
+                        break;
+                    case 'l':
+                        positions[i].x -= positions[i].speed;
+                        break;
+                }
+            }
 
-        ctx.fillText(lines[i], x, y)
-    }
+            ctx.fillText(lines[i], positions[i].x, positions[i].y);
+            if (positions[i].x < 0) {
+                positions[i].x = width + lines[i].length + 50;
+            }
+        }
+    }, 1000 / fps);
 }
